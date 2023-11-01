070e44e9d615fa4e50bf98188d79577476eb5efa
coolhub <xieyanxin93@gmail.com>
2023-11-01 11:01:41 +0800

    Update ChatBot.js

--- components/ChatBot.js
+++ components/ChatBot.js
@@ -5,7 +5,7 @@ const ChatBot = () => {
   useEffect(() => {
     MessengerRef.current.setAttribute('page_id', '172082352647924');
     MessengerRef.current.setAttribute('attribution', 'biz_inbox');
-  
+
     window.fbAsyncInit = function () {
       window.FB.init({
         xfbml: true,
@@ -14,12 +14,12 @@ const ChatBot = () => {
     };
     (function (d, s, id) {
       var js,
-        fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://connect.facebook.net/zh_CN/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
+      fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/zh_CN/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
     })(document, 'script', 'facebook-jssdk');
   }, []);
   return (
